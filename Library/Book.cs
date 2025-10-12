c80356188eeb46e15fa3f0a135ed037198bbb385
jp.connolly <jp.connolly@ulster.ac.uk>
2025-10-12 16:12:47 +0100

    Created new book class

---
+++ Library/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public string ISBN;
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+    }
+}
+
+    
+    
+
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Library;
+
+Book book = new Book();
+// This information is for one book in our library
+book.Title = "C# for beginners";
+book.Author = "BillGates";
+book.ISBN = "12345678";
+
+book.DisplayInfo();
+
+// This is another book in our library
+Book book1 = new Book();
+book1.Title = "C# Methods and classes";
+book1.Author = "Microsoft";
+book1.ISBN = "55667778";
+
+book1.DisplayInfo();
